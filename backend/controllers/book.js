5a44fddb2ac693c38274d49bf5bd51a5f14acc42
Sophie <Sophie@sfr.fr>
2025-06-02 22:28:08 +0200

    suppression ok

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,4 +1,6 @@
 const Book = require ('../models/Books');
+const fs = require ('fs');
+const path = require ('path');
 
 exports.createBook = (req, res, next)=>{
     const bookObject = JSON.parse (req.body.book);
@@ -8,6 +10,7 @@ exports.createBook = (req, res, next)=>{
 
     const book = new Book({
         ...bookObject,
+        userId:req.auth.userId,
         imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     book.save()
@@ -97,20 +100,29 @@ exports.modifyBook = (req,res,next)=>{
         .catch((error) => res.status(400).json ({error}));
 };
 
-exports.deleteBook = (req, res, next)=>{
-    Book.findOne({_id: req.params.id})
-        .then(book =>{
-            if (book.userId != req.auth.userId){
-                res.status(400).json ({message : 'Non autorisé'});
-            } else {
-                const filename = book.imageUrl.split ('/images') [1];
-                fs.unlink(path.join ('images',filename),()=>{
-                    Book.deleteOne({_id:req.params.id})
-                        .then(()=> res.status(200).json ({message : 'objet supprimé'}))
-                        .catch(error => res.status(401).json({error}))
-                });
-            }
-        })
-        .catch (error => { res.status(500).json ({error})});
+exports.deleteBook = (req, res, next) => {
+    console.log('Suppression demandée pour id :', req.params.id);
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      if (!book) {
+        return res.status(404).json({ message: 'Livre non trouvé' });
+      }
+
+      console.log('book.userId:', book.userId);
+      console.log('req.auth.userId:', req.auth.userId);
+
+      if (String(book.userId) !== String(req.auth.userId)) {
+        return res.status(403).json({ message: 'Non autorisé' });
+      }
+
+      const filename = book.imageUrl.split('/images/')[1];
+      fs.unlink(path.join('images', filename), () => {
+        Book.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Livre supprimé' }))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
 };
+
 
